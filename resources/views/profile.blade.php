19619794ad9125cef925c7a1f340e6e5f569d058
Kalpit Khamar <kkk0482@njit.edu>
2019-08-04 11:12:26 -0400

    display email on profile page and remove question IDS to display

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,6 +11,7 @@
                         <span class="font-weight-bold">First Name:</span> {{$profile->fname}}</br>
                         <span class="font-weight-bold">Last Name: </span>{{$profile->lname}}</br>
                         <span class="font-weight-bold">Body: </span>{{$profile->body}}</br>
+                        <span class="font-weight-bold">Email: </span>{{$profile->user->email}}</br>
                     </div>
                     <div class="card-footer">
                         <a class="btn btn-success float-right" href="{{ route('profile.edit', ['profile_id' => $profile->id,'user_id' => $profile->user->id]) }}">
